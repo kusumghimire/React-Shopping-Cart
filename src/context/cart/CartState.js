d58700ac7db55e10c935bf61cd60a750ba5a2a4a
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-06-27 21:07:13 +0545

    cartcontext, cartreducer is imported into CartState.js

---
+++ src/context/cart/CartState.js
@@ -0,0 +1,7 @@
+import { useReducer } from "react";
+import CartContext from "./CartContext";
+import CartReducer from './CartReducer';
+
+import { SHOW_HIDE_CART, ADD_TO_CART, REMOVE_ITEM } from "../Types";
+
+
