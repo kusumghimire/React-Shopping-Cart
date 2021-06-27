30de199d0cbf2560ff8ef3edda863604a9d8d8f9
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-06-27 21:35:30 +0545

    cartState function is added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CartState from './context/cart/CartState';
 
 ReactDOM.render(
   <React.StrictMode>
